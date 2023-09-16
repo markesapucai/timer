ef23fe58ee488ad44ee93d767e103528738167b3
Mark Leite <marktobusiness113@gmail.com>
2023-09-16 11:52:10 -0300

    feat: finish function skills

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,11 @@
 const res = document.querySelector('.res');
+const body = document.querySelector('body')
+
 const comecar = () => {
-    
+    body.style.backgroundColor = 'rgb(67, 165, 42)'
     const data = new Date('01-01-1970 00:00:00');
     let up;
-    let zero;
-
+        
     function updateClock() { 
         zeroSec = (data.getSeconds() < 10)? 0: '';
         zeroMin = (data.getMinutes() < 10)? 0: '';
@@ -12,23 +13,23 @@ const comecar = () => {
         const resul = `${zeroHor}${data.getHours()}:${zeroMin}${data.getMinutes()}:${zeroSec}${data.getSeconds()}`;
         mostraResul(resul);
     }
-
+    
     up = setInterval(() => {
         data.setSeconds(data.getSeconds() + 1);
         updateClock();
     }, 1000);
-
+    
     pausar = () => {
         clearInterval(up)
     }
     updateClock();
     reset = () => {
+        body.style.backgroundColor = 'rgb(165, 42, 42)'
         clearInterval(up)
         res.innerHTML = '00:00:00';
     }
 };
-
-
+    
 function mostraResul(resul) {
     res.innerHTML = resul;
 }
